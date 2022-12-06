00ff85cbdad2d24b005b01e4ad236c8dafc1539c
CarlosLonghi <61765253+CarlosLonghi@users.noreply.github.com>
2022-12-06 14:57:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-// EcmaScript - ES6 modules
-// NAMED IMPORT import {Timer} from './timer.js'
-// DEFAULT IMPORT import Timer from './timer.js'
+// NAMED IMPORT: import {Timer} from './timer.js'
+// DEFAULT IMPORT: import Timer from './timer.js'
 
 import Controls from './controls.js'
 import Timer from './timer.js'
@@ -15,31 +14,20 @@ import {
   secondsDisplay
 } from './elements.js'
 
-// Desestruturando variáveis de um elemento importado
-// import { elements } from './elements.js'
-// const {
-//   playButton,
-//   pauseButton,
-//   stopButton,
-//   clockButton,
-//   soundOnButton,
-//   soundOffButton,
-//   minutesDisplay,
-//   secondsDisplay
-// } = elements
 // Funções construtoras (Factory)
-
 const controls = Controls({
   playButton,
   pauseButton,
   stopButton,
   clockButton
 })
+
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
   resetControls: controls.reset
 })
+
 const sound = Sound()
 
 Events({controls, timer, sound})
